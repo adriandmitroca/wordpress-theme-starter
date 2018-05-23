69e6cc2e225d06fc343c26948d534fc1eff8a34e
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2018-05-23 08:32:12 +0200

    reformatted index.php

--- index.php
+++ index.php
@@ -4,18 +4,14 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  */
-?>
 
-<?php if ( have_posts() ) : ?>
-	<?php
-	while ( have_posts() ) :
+if ( have_posts() ) {
+	while ( have_posts() ) {
 		the_post();
-?>
-		<?php if ( is_single() || is_page() ) : ?>
-			<?php get_template_part( 'templates/content-single', get_post_type() ); ?>
-		<?php else : ?>
-			<?php get_template_part( 'templates/content', get_post_type() ); ?>
-		<?php endif ?>
-	<?php endwhile; ?>
-<?php
-endif;
+		if ( is_single() || is_page() ) {
+			get_template_part( 'templates/content-single', get_post_type() );
+		} else {
+			get_template_part( 'templates/content', get_post_type() );
+		}
+	}
+}
