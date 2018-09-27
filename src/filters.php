04b1b32f61ccaf77e280a1329f00f0e764e8f07e
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2018-09-27 16:36:42 +0200

    hide ACF settings on prod environments

--- src/filters.php
+++ src/filters.php
@@ -7,3 +7,7 @@ add_filter('get_custom_logo', function ($html) {
 add_filter('embed_oembed_html', function ($html, $url, $attr) {
     return $html !== '' ? '<div class="embed-responsive embed-responsive-16by9">' . $html . '</div>' : '';
 }, 10, 3);
+
+add_filter('acf/settings/show_admin', function () {
+    return WP_DEBUG;
+});
