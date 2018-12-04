269b13f73f590bc9cc10f5f09016a0ce073bd996
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2018-12-04 12:32:17 +0100

    reorganized styles, added utils.scss, added inherited styles for wpcf7, added loading indicator to cf7 forms

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/collapse';
+import Forms from './components/forms';
 
 class App {
   static init() {
+    (new Forms()).init();
   }
 }
 
