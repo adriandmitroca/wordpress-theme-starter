0a5d0d01626dd85c5c87bd98b3bb8d11971182ba
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2020-11-03 11:59:09 +0100

    fix: formatting in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,40 +1,41 @@
-const mix = require('laravel-mix');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
+const mix = require("laravel-mix")
+const StyleLintPlugin = require("stylelint-webpack-plugin")
 
-const DEV_DOMAIN = 'wp-boilerplate.test';
-const THEME_DIRECTORY = 'wp-boilerplate';
+const DEV_DOMAIN = "projectName.test"
+const THEME_DIRECTORY = "projectName"
 
-mix.js('js/app.js', 'dist/js')
-  .sass('sass/app.scss', 'dist/css')
-  .sourceMaps(mix.inProduction(), 'source-map')
+mix
+  .js("js/app.js", "dist/js")
+  .sass("sass/app.scss", "dist/css")
+  .sourceMaps(mix.inProduction(), "source-map")
   .options({ processCssUrls: false })
   .browserSync({
     proxy: DEV_DOMAIN,
-    files: ['dist/**/*', '**/*.php'],
+    files: ["dist/**/*", "**/*.php"]
   })
   .webpackConfig({
     module: {
       rules: [
         {
           test: /.(vue|jsx|js)$/,
-          loader: 'eslint-loader',
-          enforce: 'pre',
+          loader: "eslint-loader",
+          enforce: "pre",
           exclude: /node_modules/,
           options: {
             cache: true,
-            configFile: '.eslintrc',
-          },
-        },
-      ],
+            configFile: ".eslintrc"
+          }
+        }
+      ]
     },
     output: {
       publicPath: `/wp-content/themes/${THEME_DIRECTORY}/`,
-      chunkFilename: 'dist/js/[name].[chunkhash].js',
+      chunkFilename: "dist/js/[name].[chunkhash].js"
     },
     plugins: [
-      new StyleLintPlugin({ lintDirtyModulesOnly: !mix.inProduction() }),
+      new StyleLintPlugin({ lintDirtyModulesOnly: !mix.inProduction() })
     ],
     externals: {
-      jquery: 'jQuery',
-    },
-  });
+      jquery: "jQuery"
+    }
+  })
